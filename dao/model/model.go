41108ee09d7449ad8298fb2a6ebf3ca9377dceed
lzx <zhanxiong.liu@mthreads.com>
2023-06-19 19:06:32 +0800

    main

--- dao/model/model.go
+++ dao/model/model.go
@@ -2,13 +2,10 @@ package model
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Model struct {
 	ID        string `gorm:"primaryKey;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
